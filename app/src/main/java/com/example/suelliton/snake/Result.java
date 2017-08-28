ed51b6720087396a972272203f7b08e91510959f
suelliton <tonmelodicmetal@gmail.com>
2017-08-28 08:09:04 -0300

    sdgfsdf

--- app/src/main/java/com/example/suelliton/snake/Result.java
+++ app/src/main/java/com/example/suelliton/snake/Result.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class Result extends AppCompatActivity {
 
@@ -42,6 +45,8 @@ public class Result extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+       
 
     }
 }
